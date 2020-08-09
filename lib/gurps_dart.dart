13a819866a5eb9da0b09ec5e2b4a683b7d932b6e
mjeffw <geek.macbeer@gmail.com>
2020-08-09 09:49:10 -0400

    updated exports

--- lib/gurps_dart.dart
+++ lib/gurps_dart.dart
@@ -5,3 +5,4 @@ export 'src/units/gurps_duration.dart';
 export 'src/units/weight.dart';
 export 'src/util/core_utils.dart';
 export 'src/util/repeating_sequence.dart';
+export 'src/tables/size_speed_range.dart';
